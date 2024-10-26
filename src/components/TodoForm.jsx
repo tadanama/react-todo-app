5aeadcc4a47cad5677c416ae0302fd5ec43f6b50
Syakir Haziq <115148301+tadanama@users.noreply.github.com>
2024-10-26 22:16:10 +0800

    Use the react useState to track the state of the input

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,9 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
 function TodoForm() {
+	const [inputText, setInputText] = useState("");
+
+	function handleChange(event) {
+		const { value } = event.target;
+
+		setInputText(value);
+	}
+
 	return (
 		<form>
-			<input type="text" name="todoText" />
+			<input
+				type="text"
+				name="todoText"
+				value={inputText}
+				onChange={handleChange}
+			/>
 			<button type="submit">Add</button>
 		</form>
 	);
